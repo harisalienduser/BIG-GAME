aeac91466626f0e992fd3766be225fc23939b323
Haris-Kryptomon <haris@kryptomon.co>
2023-05-09 16:50:21 +0300

    no message

--- Assets/Ink/Demos/Basic Demo/Ink/disappearing_island.ink
+++ Assets/Ink/Demos/Basic Demo/Ink/disappearing_island.ink
@@ -1,4 +1,4 @@
-#Dr. Baciu
+#speaker:Dr. Baciu #portrait:Dr_Baciu
 Your attention, please!
 Here comes a new task for you. Are you ready to undertake it?
 
@@ -6,7 +6,7 @@ Here comes a new task for you. Are you ready to undertake it?
     + [Not really] -> esc
     
 === intro ===
-#Dr. Baciu
+#speaker:Dr. Baciu #portrait:Dr_Baciu
 "Tuvalu is sinking!"
 This phrase is so frequently used by the locals of Tuvalu, and it is true without the slightest exaggeration.
 Major Janik, would you please introduce the basics of Tuvalu to us?
@@ -14,7 +14,7 @@ Major Janik, would you please introduce the basics of Tuvalu to us?
     + [NEXT →] -> geobasic_01
 
 === geobasic_01 ===
-#Maj. Janik
+#speaker:Maj. Janik #portrait:Maj_Janik
 Tuvalu is a tiny archipelago in the Pacific Ocean, a speck of land measuring no more than 26 km², situated in Oceania, halfway between Hawaii and the Solomon Islands.
 Please refer to the map for a more exact location.
 
@@ -28,7 +28,7 @@ Please refer to the map for a more exact location.
     + [↺ BACK] -> geobasic_01
 
 === geobasic_02 ===
-#Maj. Janik
+#speaker:Maj. Janik #portrait:Maj_Janik
 Tuvalu is a Polynesian country, the 4th smallest nation in the world, with a population of just 11,000, most of whom live on Fongafale, the archipelago’s largest island, where they are packed in and fighting for space.
 Due to global warming, glaciers are melting, and sea levels are rising every year. The ocean tides are getting stronger, and more sand is being washed into the Pacific every day.
 And Doctor Kim is going to brief you what problem the above fact has led to.
@@ -37,7 +37,7 @@ And Doctor Kim is going to brief you what problem the above fact has led to.
     + [NEXT →] -> submergence_01
 
 === submergence_01 ===
-#Dr. Kim
+#speaker:Dr. Kim #portrait:Dr_Kim
 2 of Tuvalu’s 9 islands are already on the verge of disappearing under the sea, swallowed up by rising sea levels and coastal erosion.
 Most of the islands sit barely 3 m above sea level, and at its narrowest point, Fongafale stretches just 20 m across.
 During storms, waves crash into the island from the east and west, “swallowing” the land. Such a peril, isn't it?
@@ -46,7 +46,7 @@ During storms, waves crash into the island from the east and west, “swallowing
     + [NEXT →] -> submergence_02
 
 === submergence_02 ===
-#Dr. Kim
+#speaker:Dr. Kim #portrait:Dr_Kim
 As the locals remember, the sand used to stretch out far. When they swam, they could see the seabed and the coral.
 Now it is cloudy all the time and the coral are either bleaching or dying because of the rising sea temperatures.
 Many of them say they have nightmares about being gobbled up for good by the sea, which may not be as distant as it sounds, but perhaps within the next generation. Now, I hope you already have some ideas about their problem.
@@ -55,7 +55,7 @@ Many of them say they have nightmares about being gobbled up for good by the sea
     + [Take Orders] -> order
 
 === order ===
-#Dr. Baciu
+#speaker:Dr. Baciu #portrait:Dr_Baciu
 Finally, here are the tasks for you to tackle.
 1. Think about short-term solutions to decelerate the submergence of Tuvalu, so that more time can be bought to implement the long-term solutions.
 2. Think about long-term solutions to save the people of Tuvalu, as well as the local flora and fauna, to avoid the creation of refugees or a destroyed ecosystem.
--- Assets/Ink/Demos/Basic Demo/Scripts/BasicInkExample.cs
+++ Assets/Ink/Demos/Basic Demo/Scripts/BasicInkExample.cs
@@ -2,59 +2,114 @@
 using UnityEngine.UI;
 using System;
 using Ink.Runtime;
-
+using System.Collections.Generic;
+using TMPro;
+public enum characters
+{
+	Dr_Baciu,
+	Maj_Janik,
+	Dr_Kim
+}
 // This is a super bare bones example of how to play and display a ink story in Unity.
 public class BasicInkExample : MonoBehaviour {
-    public static event Action<Story> OnCreateStory;
+	public static event Action<Story> OnCreateStory;
+	private const string PORTRAIT_TAG = "portrait";
+	private const string SPEAKER_TAG = "speaker";
+	public TextMeshProUGUI displayNameText;
+	public Image portraitImg;
+	public Sprite[] characterPortraits;
 	
-    void Awake () {
+	void Awake() {
 		// Remove the default message
 		RemoveChildren();
 		StartStory();
 	}
 
 	// Creates a new Story object with the compiled story which we can then play!
-	void StartStory () {
-		story = new Story (inkJSONAsset.text);
-        if(OnCreateStory != null) OnCreateStory(story);
+	void StartStory() {
+		story = new Story(inkJSONAsset.text);
+		if (OnCreateStory != null) OnCreateStory(story);
 		RefreshView();
 	}
-	
+
 	// This is the main function called every time the story changes. It does a few things:
 	// Destroys all the old content and choices.
 	// Continues over all the lines of text, then displays all the choices. If there are no choices, the story is finished!
-	void RefreshView () {
+	void RefreshView() {
 		// Remove all the UI on screen
-		RemoveChildren ();
-		
+		RemoveChildren();
+
 		// Read all the content until we can't continue any more
 		while (story.canContinue) {
 			// Continue gets the next line of the story
-			string text = story.Continue ();
+			string text = story.Continue();
 			// This removes any white space from the text.
 			text = text.Trim();
 			// Display the text on screen!
 			CreateContentView(text);
+			
 		}
 
 		// Display all the choices, if there are any!
-		if(story.currentChoices.Count > 0) {
+		if (story.currentChoices.Count > 0) {
 			for (int i = 0; i < story.currentChoices.Count; i++) {
-				Choice choice = story.currentChoices [i];
-				Button button = CreateChoiceView (choice.text.Trim ());
+				Choice choice = story.currentChoices[i];
+				Button button = CreateChoiceView(choice.text.Trim());
 				// Tell the button what to do when we press it
-				button.onClick.AddListener (delegate {
-					OnClickChoiceButton (choice);
+				button.onClick.AddListener(delegate {
+					OnClickChoiceButton(choice);
 				});
 			}
 		}
 		// If we've read all the content and there's no choices, the story is finished!
 		else {
 			Button choice = CreateChoiceView("End of story.\nRestart?");
-			choice.onClick.AddListener(delegate{
+			choice.onClick.AddListener(delegate {
 				StartStory();
 			});
 		}
+	}
+	private void HandleTags(List<string> currentTags)
+	{
+		// loop through each tag and handle it accordingly
+		foreach (string tag in currentTags)
+		{
+			// parse the tag
+			string[] splitTag = tag.Split(':');
+			if (splitTag.Length != 2)
+			{
+				Debug.LogError("Tag could not be appropriately parsed: " + tag);
+			}
+			string tagKey = splitTag[0].Trim();
+			string tagValue = splitTag[1].Trim();
+
+			// handle the tag
+			switch (tagKey)
+			{
+				case SPEAKER_TAG:
+					displayNameText.text = tagValue;
+					break;
+				case PORTRAIT_TAG:
+					portraitImg.sprite = characterPortraits[GetCharacterIndex(tagValue)];
+					break;
+
+				default:
+					Debug.LogWarning("Tag came in but is not currently being handled: " + tag);
+					break;
+			}
+		}
+	}
+	private int GetCharacterIndex(string value)
+	{
+		foreach (characters charValue in Enum.GetValues(typeof(characters)))
+		{
+			if (charValue.ToString() == value)
+			{
+				return (int)charValue;
+				break;
+			}
+		}
+		return 0;
 	}
 
 	// When we click the choice button, tell the story to choose that choice!
@@ -68,6 +123,7 @@ public class BasicInkExample : MonoBehaviour {
 		Text storyText = Instantiate (textPrefab) as Text;
 		storyText.text = text;
 		storyText.transform.SetParent (canvas.transform, false);
+		HandleTags(story.currentTags);
 	}
 
 	// Creates a button showing the choice text
